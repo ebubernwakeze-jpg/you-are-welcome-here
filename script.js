0cbef20eac889b039d51ccb41924496a17b49b1f
Ebube <ebubernwakeze@gmail.com>
2026-01-31 21:55:31 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+// Smooth scrolling for internal links (future-proof)
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener("click", function(e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute("href"))
+      .scrollIntoView({ behavior: "smooth" });
+  });
+});
